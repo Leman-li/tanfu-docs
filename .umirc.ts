6d2918d83db5819b1bca5aaefce4cbc9d7fadc20
leman-li <leman.li.love@qq.com>
2022-01-23 13:46:30 +0800

    Update .umirc.ts

--- .umirc.ts
+++ .umirc.ts
@@ -9,5 +9,22 @@ export default defineConfig({
   mode: 'site',
   base: '/tanfu-docs',
   publicPath: '/tanfu-docs/',
+  navs: {
+    // 多语言 key 值需与 locales 配置中的 key 一致
+    'en-US': [
+      null, // null 值代表保留约定式生成的导航，只做增量配置
+      {
+        title: 'GitHub',
+        path: 'https://github.com/Leman-li/tanfu.js',
+      },
+    ],
+    'zh-CN': [
+      null, // null 值代表保留约定式生成的导航，只做增量配置
+      {
+        title: 'GitHub',
+        path: 'https://github.com/Leman-li/tanfu.js',
+      },
+    ],
+  },
   // more config: https://d.umijs.org/config
 });
